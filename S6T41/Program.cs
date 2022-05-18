d25b759207f3cccc4690ef4913c5c1baf386a4f1
GarryLit <garry_lit@mail.ru>
2022-05-19 01:56:48 +0300

    123

---
+++ S6T41/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write("Какое количество чисел Вы хотите ввести? - ");
+int amountNumbers = Convert.ToInt32(Console.ReadLine());
+
+int numberPositive = 0; 
+
+for(int i = 1; i <= amountNumbers; i++ )
+{
+    Console.Write($"Введите число {i} ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber > 0)
+        numberPositive ++;
+}
+
+System.Console.WriteLine($"Вы ввели {numberPositive} положительных числа");
---
+++ S6T43/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+// В ответе судя по всему опечатка. Правильно (-0,5; -0,5)
+
+double InputNumber(string name)
+{
+    System.Console.Write($"Введите  {name} = ");    
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;        
+}
+
+double b1 = InputNumber("b1");
+double b2 = InputNumber("b2");
+double k1 = InputNumber("k1");
+double k2 = InputNumber("k2");
+
+double x = (b1 - b2) / (k2 - k1);
+double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+System.Console.WriteLine(y);
+System.Console.WriteLine(x);
+
